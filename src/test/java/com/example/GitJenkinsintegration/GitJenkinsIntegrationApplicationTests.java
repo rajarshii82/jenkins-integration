8de9953e383d66a159712f1fbfce06b974ac3df4
rajarshii82 <rajarshib13@gmail.com>
2021-06-12 22:37:04 -0400

    firt base addistion added to git

---
+++ src/test/java/com/example/GitJenkinsintegration/GitJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitJenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
